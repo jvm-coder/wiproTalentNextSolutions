4f79cafaa739642de045e954531c0df618b86b11
Aakash kumar sahoo <56172886+jvm-coder@users.noreply.github.com>
2022-07-04 15:45:32 +0530

    Update RestrictingAndSortingData.sql

--- Milestone-2/RDBMS-SQL-JDBC/RestrictingAndSortingData.sql
+++ Milestone-2/RDBMS-SQL-JDBC/RestrictingAndSortingData.sql
@@ -4,16 +4,31 @@ select last_name, salary from departments where salary>12000;
 
 -- 2.  select last_name, salary from departments where salary>12000;
 
-select last_name, dept_id from departments where emp_id = 176;
+select last_name, dept_id 
+from departments 
+where emp_id = 176;
 
 -- 3.  To find high-salary and low-salary employees. Create a query to display the last name and salary for any employee 
 --     whose salary is not in the range of $5,000 to $12,000
 
-
+select last_name, salary 
+from departments 
+where salary not between 5000 and 12000;
 
 -- 4.  Create a report to display the last name, job ID, and hire date for employees with the last names of Matos and Taylor. 
 --     Order the query in ascending order by the hire date.
+
+select last_name, dept_id, hire_date 
+from departments 
+where last_name in ('Matos','Taylor') 
+order by hire_date;
+
 -- 5.  Display the last name and department ID of all employees in departments 20 or 50 in ascending alphabetical order by name.
+
+select last_name, dept_id 
+from departments 
+where dept_id=20 or dept_id=50;
+
 -- 6.  List employees who earn between $5,000 and $12,000, and are in department 20 or 50. Label the columns as
 --     Employee and Monthly Salary, respectively.	
 -- 7.  Create a report that displays the last name and hire date for all employees who were hired in 1994.
