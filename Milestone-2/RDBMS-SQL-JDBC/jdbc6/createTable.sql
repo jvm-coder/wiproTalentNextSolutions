2bf2cc37a9bb7fe64096edca8474ab9c63c83562
Aakash kumar sahoo <56172886+jvm-coder@users.noreply.github.com>
2022-07-05 11:08:57 +0530

    Update createTable.sql

--- Milestone-2/RDBMS-SQL-JDBC/jdbc6/createTable.sql
+++ Milestone-2/RDBMS-SQL-JDBC/jdbc6/createTable.sql
@@ -1,1 +1,6 @@
-create table stud (rollNo number(4) primary key, studentName varchar(20) not null, standard varchar(2) not null, dob date, fees number(9,2));
+create table stud (rollNo number(4) primary key, 
+        studentName varchar(20) not null, 
+        standard varchar(2) not null, 
+        dob date, 
+        fees number(9,2)
+        );
