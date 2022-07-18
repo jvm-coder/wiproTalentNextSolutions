6cf482ba1dcf85321485c2e78860ba871c943ac3
Aakash Kumar Sahoo <rksahoo2806@gmail.com>
2022-07-18 15:03:49 +0530

    added prob1

---
+++ Milestone-3/Servlets and JSP/prob1/src/Hello.java
@@ -0,0 +1,25 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public Hello() {}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.print("<html><body>");
+		out.print("<h3>My first Servlet program</h3>");
+		out.print("</body></html>");
+	}
+
+}
